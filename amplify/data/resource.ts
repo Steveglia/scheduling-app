6443dd16616b14378128f626000df99d025b9129
Steveglia <steveglia1@gmail.com>
2025-02-06 15:18:21 +0000

    Sandbox Impl

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,10 +7,12 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    }).authorization(allow => [allow.owner()]),
+  Event: a
+  .model({
+    title: a.string(),
+    description: a.string(),
+    location: a.string(),
+  }).authorization(allow => [allow.owner()]), 
 });
 
 export type Schema = ClientSchema<typeof schema>;
